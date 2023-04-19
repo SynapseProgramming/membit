bae8281373fa8fa2cb38dfddf8a763df3e6bb7e3
Roald Ong <roald.ong@gmail.com>
2023-04-19 17:21:24 +0800

    Added in template database class

---
+++ lib/isardb.dart
@@ -0,0 +1,20 @@
+import 'package:isar/isar.dart';
+import 'package:membit/entities/card.dart';
+import 'package:membit/entities/deck.dart';
+import 'package:path_provider/path_provider.dart';
+
+class IsarDb {
+  late Future<Isar> db;
+  IsarDb() {
+    db = openDB();
+  }
+  Future<Isar> openDB() async {
+    if (Isar.instanceNames.isEmpty) {
+      final dir = await getApplicationDocumentsDirectory();
+      return await Isar.open([CardSchema, DeckSchema],
+          inspector: true, directory: dir.path);
+    }
+    return Future.value(Isar.getInstance());
+  }
+  
+}
