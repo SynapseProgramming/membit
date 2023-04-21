015c53e73418b4781ff09804b8c0960c1943a0b6
Roald Ong <roald.ong@gmail.com>
2023-04-21 14:35:40 +0800

    Added in a navbar for the decks widget

--- lib/maindeck.dart
+++ lib/maindeck.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Maindeck extends StatefulWidget {
-
-
-
   @override
   State<Maindeck> createState() => _MaindeckState();
 }
@@ -11,6 +8,26 @@ class Maindeck extends StatefulWidget {
 class _MaindeckState extends State<Maindeck> {
   @override
   Widget build(BuildContext context) {
-    return Padding(child: Text("hello"), padding: EdgeInsets.all(8.0));
+    return Scaffold(
+        appBar: AppBar(
+            leading: IconButton(
+                onPressed: () {
+                  print("Add button pressed");
+                },
+                icon: Icon(Icons.add)),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    print("Delete button pressed");
+                  },
+                  icon: Icon(Icons.delete)),
+            ],
+            title: Center(
+              child: const Text(
+                'Decks',
+              ),
+            )),
+        body:
+            Padding(child: const Text("hello"), padding: EdgeInsets.all(8.0)));
   }
 }
