27af8aed03f0366e67dff5943379c1cd2f605591
Roald Ong <roald.ong@gmail.com>
2023-05-03 16:51:12 +0800

    Added in a navbar

--- lib/deckmenuscreen.dart
+++ lib/deckmenuscreen.dart
@@ -4,17 +4,35 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:membit/router.gr.dart';
 
-
-
 @RoutePage()
 class DeckMenuScreen extends StatelessWidget {
   const DeckMenuScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
-      child: Text("niggaa"),
-    );
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.green,
+            leading: IconButton(
+                onPressed: () {
+                  print("Add button pressed");
+                },
+                icon: const Icon(Icons.add)),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    print("Delete button pressed");
+                  },
+                  icon: const Icon(Icons.delete)),
+            ],
+            title: const Center(
+              child: Text(
+                'Card',
+              ),
+            )),
+        body: const Padding(
+          padding: EdgeInsets.all(16),
+          child: Text("niggaa"),
+        ));
   }
 }
