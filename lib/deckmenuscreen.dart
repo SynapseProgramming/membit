d6bdae87054833778f079abcb9bdc09c78e97904
Roald Ong <roald.ong@gmail.com>
2023-05-03 15:22:46 +0800

    Added in deckmenu screen

---
+++ lib/deckmenuscreen.dart
@@ -0,0 +1,20 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:membit/router.gr.dart';
+
+
+
+@RoutePage()
+class DeckMenuScreen extends StatelessWidget {
+  const DeckMenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(16),
+      child: Text("niggaa"),
+    );
+  }
+}
