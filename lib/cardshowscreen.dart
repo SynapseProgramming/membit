c0d7154da4d02d0d6527dad3a5b52f394625325e
Roald Ong <roald.ong@gmail.com>
2023-05-17 12:56:27 +0800

    Added in iteration to next card, and button

--- lib/cardshowscreen.dart
+++ lib/cardshowscreen.dart
@@ -25,7 +25,6 @@ class _CardShowScreenState extends State<CardShowScreen> {
 
   Future<void> getCards(IsarDb db) async {
     if (fired == false) {
-      // cards.clear();
       cards = await db.getCardsFor(widget.currentDeck);
       setState(() {
         fired = true;
@@ -66,59 +65,83 @@ class _CardShowScreenState extends State<CardShowScreen> {
             ),
           )),
       body: Visibility(
-        visible: cards.isNotEmpty,
-        child: cards.length > 0
-            ? Column(
-                children: [
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Center(
-                    child: GestureDetector(
-                      onTap: () => setState(() {
-                        show = true;
-                      }),
-                      child: Container(
-                        width: 300,
-                        height: 500,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 1.0,
-                          ),
-                        ),
-                        child: Column(children: [
-                          Text(
-                            cards[current_index].front,
-                            style: TextStyle(
-                              fontSize: 40,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'Arial',
+          visible: cards.isNotEmpty,
+          child: cards.length > 0 && current_index < cards.length
+              ? Column(
+                  children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Center(
+                      child: GestureDetector(
+                        onTap: () => setState(() {
+                          show = true;
+                        }),
+                        child: Container(
+                          width: 300,
+                          height: 500,
+                          decoration: BoxDecoration(
+                            border: Border.all(
                               color: Colors.black,
+                              width: 1.0,
                             ),
                           ),
-                          SizedBox(
-                            height: 100,
-                          ),
-                          Visibility(
-                            visible: show,
-                            child: Text(cards[current_index].back,
-                                style: TextStyle(
-                                  fontSize: 40,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'Arial',
-                                  color: Colors.green,
-                                )),
-                          ),
-                        ]),
+                          child: Column(children: [
+                            Text(
+                              cards[current_index].front,
+                              style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Arial',
+                                color: Colors.black,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 100,
+                            ),
+                            Visibility(
+                              visible: show,
+                              child: Text(cards[current_index].back,
+                                  style: TextStyle(
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Arial',
+                                    color: Colors.green,
+                                  )),
+                            ),
+                          ]),
+                        ),
                       ),
                     ),
-                  )
-                ],
-              )
-            : Text('0'),
-        replacement: const Text("NO CARDS"),
-      ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Visibility(
+                      visible: show,
+                      child: ElevatedButton(
+                        onPressed: () => {
+                          setState(
+                            () {
+                              show = false;
+                              current_index++;
+                              complete_ratio = current_index / cards.length;
+                            },
+                          )
+                        },
+                        child: Text('next'),
+                        style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(200, 50),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            backgroundColor: Colors.green),
+                      ),
+                    ),
+                  ],
+                )
+              // TODO: the complete deck widget should have a tick, and a recommended return to home screen
+              : Text('Deck Complete!'),
+          replacement: const Text("NO CARDS")),
     );
   }
 }
