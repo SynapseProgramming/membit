be44b0aaa5e547152a26c94fde8972a5e808dfed
Roald Ong <roald.ong@gmail.com>
2023-06-23 23:16:09 +0800

    Added in calendar

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,10 +1,16 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:table_calendar/table_calendar.dart';
+
 import 'package:flutter/material.dart';
 
 @RoutePage()
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: Text("hello nigga"));
+    return TableCalendar(
+      firstDay: DateTime.utc(2010, 10, 16),
+      lastDay: DateTime.utc(2030, 3, 14),
+      focusedDay: DateTime.now(),
+    );
   }
 }
