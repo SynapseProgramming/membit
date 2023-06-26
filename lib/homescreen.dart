c2eb32f094229088bc2436604b5af48e7dac6061
Roald Ong <roald.ong@gmail.com>
2023-06-26 14:30:47 +0800

    Fixed calendar crashing issue

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -4,13 +4,36 @@ import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime _focusedDay = DateTime.now();
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+
   @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-      firstDay: DateTime.utc(2010, 10, 16),
-      lastDay: DateTime.utc(2030, 3, 14),
-      focusedDay: DateTime.now(),
+    return Scaffold(
+      appBar: AppBar(title: const Center(child: Text('Home'))),
+      body: TableCalendar(
+        firstDay: DateTime.utc(2010, 10, 16),
+        lastDay: DateTime.utc(2030, 3, 14),
+        calendarFormat: _calendarFormat,
+        focusedDay: _focusedDay,
+        onPageChanged: (focus) {
+          _focusedDay = focus;
+        },
+        onFormatChanged: (format) {
+          if (_calendarFormat != format) {
+            // Call `setState()` when updating calendar format
+            setState(() {
+              _calendarFormat = format;
+            });
+          }
+        },
+      ),
     );
   }
 }
